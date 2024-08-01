a0df7c280bf2d28ff5c455cb213cc66293420155
Javier Reyes <javierreyes@MacBook-Pro-de-mac.local>
2024-08-01 14:19:24 -0500

    definicion de tipos de datos

---
+++ bases_dart/02_tipos_de_datos.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Números
+  int empleados = 10;
+  double pi     = 3.141595;
+  var numero    = 1.0; //double
+
+  print('$empleados - $pi');
+
+  // String Cadenas de caracteres
+  String nombre = 'Tony';
+  print(nombre);
+  print(nombre[0]);
+  print(nombre[nombre.length - 1]);
+}
---
+++ bases_dart/04_listas.dart
@@ -0,0 +1,15 @@
+void main(){
+
+    List<int> numeros = [1,2,3,4,5];
+    print(numeros);
+
+    numeros.add(6);
+    print(numeros);
+
+    // Tamaño fijo
+    List masNumeros =  List(10);
+    print(masNumeros);
+
+
+
+}
