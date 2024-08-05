98b3a4e33b33325a7e6874be6a79233104468a46
Javier Reyes <javierreyes@MacBook-Pro-de-mac.local>
2024-08-05 11:17:43 -0500

    uso de metodos en los metodos

--- 02_apps/a_count/a_count/lib/src/pages/contador_page.dart
+++ 02_apps/a_count/a_count/lib/src/pages/contador_page.dart
@@ -9,33 +9,55 @@ class ContadorPage extends StatefulWidget {
 
 class _ContadorPageState extends State<ContadorPage> {
   final _estiloTexto = TextStyle(fontSize: 25);
+  final _separacionBotones = 6.0;
   int _conteo = 10;
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Statefull'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Numero de clicks:', style: _estiloTexto),
-            Text('$_conteo', style: _estiloTexto),
-          ],
+        appBar: AppBar(
+          title: Text('Statefull'),
+          centerTitle: true,
         ),
-      ),
-      //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, // como cambiar la ubicacion del boton
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            _conteo++;
-          });
-        },
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Numero de clicks:', style: _estiloTexto),
+              Text('$_conteo', style: _estiloTexto),
+            ],
+          ),
+        ),
+        //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, // como cambiar la ubicacion del boton
+        floatingActionButton: _crearBotones());
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        SizedBox(width: 30),
+        FloatingActionButton(child: Icon(Icons.add), onPressed: _agregar),
+        Expanded(child:  SizedBox()),
+        FloatingActionButton(child: Icon(Icons.remove), onPressed: _quitar),
+        SizedBox(width:  _separacionBotones ),
+        FloatingActionButton(child: Icon(Icons.exposure_zero), onPressed: _setDefault),
+      ]);
+  }
+
+  void _agregar(){
+    _conteo++;
+    setState(() {});// redibuja en tiempo real
+  }
+
+  void _quitar(){
+    setState(() => _conteo--  );
+  }
+
+  void _setDefault(){
+    setState(() {
+      _conteo = 10;
+    });
   }
 }
