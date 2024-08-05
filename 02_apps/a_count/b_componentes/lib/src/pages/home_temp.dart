f61f546fa604b240c0be81104502b57ccb982e64
Javier Reyes <javierreyes@MacBook-Pro-de-mac.local>
2024-08-05 12:26:09 -0500

    recorrer lsita

--- 02_apps/a_count/b_componentes/lib/src/pages/home_temp.dart
+++ 02_apps/a_count/b_componentes/lib/src/pages/home_temp.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class HomeTemp extends StatelessWidget {
-  const HomeTemp({super.key});
+
+
+
+  //const HomeTemp({super.key});
+  final opciones = ['Uno','Dos','Tres','Cuatro','Cinco'];
 
   @override
   Widget build(BuildContext context) {
@@ -11,23 +15,44 @@ class HomeTemp extends StatelessWidget {
         title: Text('Componentes Temp'),
       ),
       body: ListView(
-        children: <Widget> [
-          ListTile(
-            title:  Text( 'ListTitle')
-          ),
-          Divider(),
-                    ListTile(
-            title:  Text( 'ListTitle')
-          ),
-          Divider(),
-          ListTile(
-            title:  Text( 'ListTitle')
-          ),
-          
-
-
-        ],
+       // children: _crearItems()
+       children: _crearItemsCorto()
       ),
     );
   }
+
+
+    List<Widget>_crearItems(){
+      List<Widget> lista = [];
+
+      for (String opt in opciones) {
+        final tempWidget = ListTile(
+          title: Text(opt),
+        );
+        lista..add(tempWidget)
+             ..add(Divider());
+      }
+      return lista;
+    }
+
+    List<Widget>_crearItemsCorto(){
+      List<Widget> lista = [];
+      return opciones.map((item){
+
+          return Column(
+            children: <Widget>[
+              ListTile (
+                title: Text(item),
+                subtitle: Text('Cualquier cosa'),
+                leading: Icon(Icons.account_balance_wallet),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap: (){},
+              ),
+              Divider()
+            ],
+          );
+
+        }
+      ).toList(); 
+    }
 }
