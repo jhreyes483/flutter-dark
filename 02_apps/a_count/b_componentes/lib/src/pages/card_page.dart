e50e59ed5816b283b582fe47261e297e8219ecd5
Javier Reyes <javierreyes@MacBook-Pro-de-mac.local>
2024-08-05 16:39:13 -0500

    mejoras en card

--- 02_apps/a_count/b_componentes/lib/src/pages/card_page.dart
+++ 02_apps/a_count/b_componentes/lib/src/pages/card_page.dart
@@ -4,44 +4,67 @@ class CardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Cards')
+        appBar: AppBar(title: Text('Cards')),
+        body: ListView(
+          //padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 10.0),
+          padding: EdgeInsets.all(10.0),
+          children: <Widget>[
+            _cardTipo1(),
+            SizedBox(height: 30.0),
+            _cardTipo2()
+          ],
+        ), 
+        floatingActionButton: FloatingActionButton( // boton para regresar
+        onPressed: () { 
+          Navigator.pop(context);
+        },
+        child: Icon( Icons.add_location)
       ),
-      body : ListView(
-        //padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 10.0),
-        padding: EdgeInsets.all(10.0),
-        children:  <Widget> [
-          _cardTipo1(),
-          SizedBox(height: 30.0),
-          _cardTipo2()
-        ],
-      ) 
-    );
+        
+        
+        );
   }
 
-Widget _cardTipo2() {
-  return Card(
-    child: Column(
-      children: [
-        // Asegúrate de proporcionar un tamaño específico para la imagen
-        Image.network(
-          'https://www.w3schools.com/w3images/lights.jpg',
-          fit: BoxFit.cover, // Ajusta el tamaño de la imagen para que se ajuste al contenedor
-        ),
-        // Asegúrate de envolver el texto en un contenedor adecuado
-        Padding(
-          padding: const EdgeInsets.all(10.0), // Ajusta el espaciado alrededor del texto
-          child: Text('Texto de abajo'),
-        ),
-      ],
-    ),
-  );
-}
+  Widget _cardTipo2() {
+    final card = Container(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(
+                10.0), // Ajusta el espaciado alrededor del texto
+            child: Text('Texto de abajo'),
+          ),
+        ],
+      ),
+    );
 
+    return Container(
+      decoration    : BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10.0,
+            spreadRadius: 2.0,
+            offset: Offset(2.0, 10.0)
+          )
+        ]
+      ),
+      child: ClipRRect( // quita todo lo que este porfuera del contenedor
+        borderRadius: BorderRadius.circular(30.0),
+        child: card,
+      ),
+    ); 
+  }
 
   Widget _cardTipo1() {
-
+    
     return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0) 
+      ),
       child: Column(
         children: [
           ListTile(
@@ -52,11 +75,11 @@ Widget _cardTipo2() {
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-            TextButton(
+              TextButton(
                 child: Text('Cancelar'),
                 onPressed: () {},
               ),
-            TextButton(
+              TextButton(
                 child: Text('Ok'),
                 onPressed: () {},
               ),
@@ -65,9 +88,6 @@ Widget _cardTipo2() {
         ],
       ),
     );
+
   }
-
 }
-
-
-
