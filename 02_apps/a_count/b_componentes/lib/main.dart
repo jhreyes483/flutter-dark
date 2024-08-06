167092508d8e27eaa9629b0bf3224fcc1d140c26
Javier Reyes <javierreyes@MacBook-Pro-de-mac.local>
2024-08-06 12:37:26 -0500

    configuracion de input datapicker

--- 02_apps/a_count/b_componentes/lib/main.dart
+++ 02_apps/a_count/b_componentes/lib/main.dart
@@ -1,11 +1,13 @@
-//import 'package:b_componentes/src/pages/alert_page.dart';
-//import 'package:b_componentes/src/pages/avatar_page.dart';
+
+import 'package:b_componentes/src/pages/alert_page.dart';
 import 'package:b_componentes/src/pages/home.dart';
 import 'package:b_componentes/src/routes/routes.dart';
-//import 'package:b_componentes/src/pages/home_temp.dart';
+
 import 'package:flutter/material.dart';
-//import 'src/pages/home_temp.dart';
-//import 'package:b_componentes/src/pages/alert_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+
+
 
 void main() => runApp(const MyApp());
 
@@ -18,15 +20,22 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Componentes App',
         // home: HomePage(),
-
-        initialRoute: 'home',
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],  
+        supportedLocales: [
+          const Locale('en', 'US'), // English
+          const Locale('es', 'ES'),
+        ],
+        initialRoute:  '/',
         routes: getApplicationRoutes(),
         onGenerateRoute: (RouteSettings settings) {
-          /*
+          
           return MaterialPageRoute(
             builder: (BuildContext context) => AlertPage() 
           );
-          */
+          
         }
       );
   }
--- 02_apps/a_count/b_componentes/lib/src/pages/input_page.dart
+++ 02_apps/a_count/b_componentes/lib/src/pages/input_page.dart
@@ -9,7 +9,11 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  String _nombre ="";
+  String _nombre    = '';
+  String _email     = '';
+  String _password  = '';
+  String _fecha     = '';
+  TextEditingController _inputFieldDateController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +28,14 @@ class _InputPageState extends State<InputPage> {
         children: [
           _crearImput(),
           Divider(),
-          _crearPersona()
+          _crearEmail(),
+          Divider(),
+          _crearPassword(),
+          Divider(),
+          _crearFecha(context),
+          Divider(),
+          _crearPersona(),
+
         ],
       ),
     );
@@ -56,7 +67,93 @@ class _InputPageState extends State<InputPage> {
   
   Widget _crearPersona() {
     return ListTile(
-      title: Text('Nombre es: $_nombre'),
+      title    : Text('Nombre es: $_nombre'),
+      subtitle : Text('Email: $_email')
     );
+  }
+  
+  Widget _crearEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border   : OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        counter    : Text('Letras ${_nombre.length} '),
+        hintText   : 'Email',
+        labelText  : 'Email',
+        helperText : 'Solo es el email',
+        suffixIcon : Icon(Icons.alternate_email),
+        icon       : Icon(Icons.email)
+      ),
+      onChanged: (valor){
+        _email = valor;
+        setState(() {});
+      },
+
+    );
+  }
+  
+  Widget _crearPassword() {
+    return TextField(
+      obscureText: true,
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border   : OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        counter    : Text('Letras ${_nombre.length} '),
+        hintText   : 'Password',
+        labelText  : 'Password',
+        helperText : 'Solo es el nombre',
+        suffixIcon : Icon(Icons.lock),
+        icon       : Icon(Icons.password)
+      ),
+      onChanged: (valor){
+        _password = valor;
+        setState(() {});
+      },
+    );
+  }
+  
+  Widget _crearFecha(BuildContext context) {
+    return TextField(
+
+      enableInteractiveSelection: false,
+      controller: _inputFieldDateController,
+      decoration: InputDecoration(
+        border   : OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        counter    : Text('Letras ${_nombre.length}'),
+        hintText   : 'Fecha de nacimiento',
+        labelText  : 'Fecha de nacimiento',
+        suffixIcon : Icon( Icons.perm_contact_calendar ),
+        icon       : Icon( Icons.calendar_today )
+      ),
+      onTap: (){
+        FocusScope.of(context).requestFocus(new FocusNode()); // desactiva el autofocus
+        _selectDate( context );
+      },
+    );
+    
+  }
+  
+  void _selectDate(BuildContext context) async {
+    // Mostrar el selector de fecha
+    DateTime? picked = await showDatePicker(
+      context: context, 
+      initialDate: new DateTime.now(),
+      firstDate: new DateTime(2024), 
+      lastDate: new DateTime(2025),
+      locale: Locale('es','ES')
+    );
+
+    if(picked != null){
+      setState(() {
+        _fecha = picked.toString();
+        _inputFieldDateController.text = _fecha;
+      });
+    }
   }
 }
